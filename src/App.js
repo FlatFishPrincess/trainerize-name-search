36db38ee56c6e8f87a98cae1a055b71db1b42d40
Jiweon Park <jiweon.park@tell.com>
2021-08-13 12:30:13 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import logo from './assets/logo_trainerize.png';
+import './style.scss';
 
-function App() {
+const BASE_FETCH_URL = "https://api.agify.io/";
+const localStorage = window.localStorage;
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  // set timeout to reduce redundant API calls
+  useEffect(() => {
+    const timeOutId = setTimeout(() => handleProfileByName(name), 500);
+    return () => clearTimeout(timeOutId);
+  }, [name]);
+
+  const handleChangeInput = e => {
+    setName(e.target.value);
+    // reset profile data if input is empty
+    if(e.target.value === "") {
+      setProfile({});
+    }
+  }
+
+  // if user enter press, fetching data
+  const handleKeyPress = e => {
+    if(e.key === 'Enter'){
+      console.log('name', name);
+      handleProfileByName(name);
+    }
+  }
+
+  const handleSetProfileStorage = (newProfile) => {
+    const profileData = localStorage.getItem('profile');
+    let data = JSON.parse(profileData);
+    if(data == null) { data = {} };
+    data = { ...data, [newProfile.name]: newProfile };
+    localStorage.setItem('profile', JSON.stringify(data));
+  }
+
+  // if the name already exists in profile, return data.
+  const validateDuplicateName = (name) => {
+    const profileData = localStorage.getItem('profile');
+    let data = JSON.parse(profileData);
+    if(data == null) { return false };
+    return data[name];
+  }
+
+  const handleProfileByName = (name) => {
+    // see if there's any duplicate
+    const profileFromCache = validateDuplicateName(name);
+    if(profileFromCache) {
+      setProfile(profileFromCache);
+    } else {
+      handleFetchAgeByName(name);
+    }
+  }
+
+  // call API
+  const handleFetchAgeByName = async (name) => {
+    if (!name) { return; }
+    setLoading(true);
+    try {
+      const result = await fetch(`${BASE_FETCH_URL}?name=${name}`);
+      if(result.status === 200) {
+        const { name, age, count } = await result.json();
+        setProfile({ name, age, count });
+        handleSetProfileStorage({ name, age, count });
+      } else {
+        const errMsg = 'Unexpected error occurs';
+        throw errMsg;
+      }
+    } catch (e) {
+      console.log('err msg', e);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="app-content">
+        <img
+          src={logo}
+          width="150px"
+          height="150px"
+          className="logo"
+          alt="trainerize-logo"
+        />
+        <div className="control">
+          <label htmlFor="name" className="control__label">Name</label>
+          <input
+            type="text"
+            name="name"
+            className="control__input"
+            placeholder="Please enter a name to get the estimated age"
+            onChange={handleChangeInput}
+            value={name}
+            onKeyPress={handleKeyPress}
+          />
+        </div>
+        <hr className="divider" />
+        <div className="result-container">
+          <div className="result__row">
+            <span className="result__label">Estimated age</span>
+            <span className="result__value">{profile.age && profile.age}</span>
+          </div>
+          <div className="result__row">
+            <span className="result__label">Count</span>
+            <span className="result__value">{profile.count && profile.count}</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
